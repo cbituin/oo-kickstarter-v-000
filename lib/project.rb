4033be938c303301c8a07f28043c9fd75b1d6c80
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-12-27 17:23:46 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+  
+  attr_accessor :title
+  
+  @@backers = []
+  
+  def initialize(title)
+    @title = title
+  end
+    
+  def add_backer(backer)
+    @@backers << backer
+    backer.back_project(self)
+  end
+  
+  def backers
+    @@backers
+  end
+end
